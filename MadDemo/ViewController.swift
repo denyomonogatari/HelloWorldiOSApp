8d3ae0bda0c35087930e45096f500af8142255f5
denyomonogatari <dvictor4@calstatela.edu>
2018-09-12 23:03:15 -0700

    color changes randomly on button press

--- MadDemo/ViewController.swift
+++ MadDemo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textLabel: UILabel!
     
@@ -28,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // change the text when enter is pressed while text field is selected
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+        return true
+    }
     
     @IBAction func didTapChangeTextButton(_ sender: Any) {
         textLabel.text = textField.text
@@ -36,13 +43,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapChangeViewColorButton(_ sender: Any) {
-        view.backgroundColor = UIColor.yellow
+        view.backgroundColor = getRandomColor()
     }
     
     @IBAction func didTapChangeTextColorButton(_ sender: Any) {
-        textLabel.textColor = UIColor.green
+        textLabel.textColor = getRandomColor()
     }
-    
     
     @IBAction func onResetGesture(_ sender: Any) {
         textLabel.text = "Hello!"
@@ -51,6 +57,18 @@ class ViewController: UIViewController {
         
     }
     
+    // generate a random color 
+    func getRandomColor() -> UIColor{
+        
+        var randomRed:CGFloat = CGFloat(drand48())
+        
+        var randomGreen:CGFloat = CGFloat(drand48())
+        
+        var randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
+    }
     
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# HelloWorldiOSApp
+## HelloWorldiOSAp
+
+
+### App Description
+Allow user to change text, change the view color, and change the text color.
+Extras: 
+The color of the text and view changes randomly when respective button is pressed. 
+Enter can be pressed to update the label with the text from the text field. 
+
+### App Walk-though
+<img src="https://imgur.com/WWWh3Pt" width=200><br>
+
+### Required User Stories
+- [x] 1. User sees custom text in a label - Hello from {name}!
+- [x] 2. User see's custom background color.
+- [x] 3. User can tap a button to change the text color of the label.
+
+### Optional User Stories
+- [x] 1. User can tap a button to change the color of the background view.
+- [x] 2. User can tap a button to change the text string of the label - Goodbye 👋.
+- [x] 3. User can tap on the background view to reset all views to default settings.
+- [x] 4. User can update the label text with custom text entered into the text field.
+   - [x] a. User can enter text into a text field using the keyboard.
+   - [x] b. User can tap the "Change text string" button to update the label with the text from the text field.
+   - [x] c. If the text field is empty, update label with default text string.
+   - [x] d. The keyboard is dismissed after the button has been tapped.
+
